bf1a8b5a36aa6cad126a0a5f330ecca02338b8a6
BagarBilla <piyushpra33@gmail.com>
2026-04-30 12:43:46 +0530

    scroll to top fixed

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import ReactDOM from "react-dom/client";
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
 import AppProvider from './context/AppProvider';
-
 import { BrowserRouter } from "react-router-dom";
+import ScrollToTop from "./MainComponents/ScrollToTop.jsx"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-      <BrowserRouter>
+    <BrowserRouter>
+      <ScrollToTop /> 
+      
       <AppProvider>
-      <App />
+        <App />
       </AppProvider>
-    </BrowserRouter>
 
-  </StrictMode>,
-)
+    </BrowserRouter>
+  </StrictMode>
+);
